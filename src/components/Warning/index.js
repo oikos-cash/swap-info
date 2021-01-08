30a36849f00b05e669862877f8d25a2c9502adc7
Oikos.cash <albert@oikos.cash>
2021-01-07 22:35:08 -0400

    ERC20 --> TRC20 / Ethereum --> Tron / Etherscan --> Tronscan

--- src/components/Warning/index.js
+++ src/components/Warning/index.js
@@ -38,20 +38,20 @@ export default function Warning({ type, show, setShow, address }) {
   const textContent = below800 ? (
     <div>
       <Text fontWeight={500} lineHeight={'145.23%'} mt={'10px'}>
-        Anyone can create and name any ERC20 token on Ethereum, including creating fake versions of existing tokens and
+        Anyone can create and name any TRC20 token on Tron, including creating fake versions of existing tokens and
         tokens that claim to represent projects that do not have a token.
       </Text>
       <Text fontWeight={500} lineHeight={'145.23%'} mt={'10px'}>
-        Similar to Etherscan, this site automatically tracks analytics for all ERC20 tokens independent of token
-        integrity. Please do your own research before interacting with any ERC20 token.
+        Similar to Tronscan, this site automatically tracks analytics for all TRC20 tokens independent of token
+        integrity. Please do your own research before interacting with any TRC20 token.
       </Text>
     </div>
   ) : (
     <Text fontWeight={500} lineHeight={'145.23%'} mt={'10px'}>
-      Anyone can create and name any ERC20 token on Ethereum, including creating fake versions of existing tokens and
-      tokens that claim to represent projects that do not have a token. Similar to Etherscan, this site automatically
-      tracks analytics for all ERC20 tokens independent of token integrity. Please do your own research before
-      interacting with any ERC20 token.
+      Anyone can create and name any TRC20 token on Tron, including creating fake versions of existing tokens and
+      tokens that claim to represent projects that do not have a token. Similar to Tronscan, this site automatically
+      tracks analytics for all TRC20 tokens independent of token integrity. Please do your own research before
+      interacting with any TRC20 token.
     </Text>
   )
 
@@ -75,7 +75,7 @@ export default function Warning({ type, show, setShow, address }) {
                 href={'https://tronscan.io/#/transaction/' + address.substring(2)}
                 target="_blank"
               >
-                View {type === 'token' ? 'token' : 'pair'} contract on Etherscan
+                View {type === 'token' ? 'token' : 'pair'} contract on Tronscan
               </Link>
             </Hover>
             <RowBetween style={{ marginTop: '20px' }}>
@@ -95,7 +95,7 @@ export default function Warning({ type, show, setShow, address }) {
                 href={'https://tronscan.io/#/transaction/' + address.substring(2)}
                 target="_blank"
               >
-                View {type === 'token' ? 'token' : 'pair'} contract on Etherscan
+                View {type === 'token' ? 'token' : 'pair'} contract on Tronscan
               </Link>
             </Hover>
             <ButtonDark color={'#f82d3a'} style={{ minWidth: '140px' }} onClick={() => setShow(false)}>
